b8dace8e197a39a3a2667de70187fdb5cfb8252c
Fábio Akira Yoshida <fabioseno@gmail.com>
2015-03-01 19:42:37 -0300

    Adding favorite list

--- controllers/player.js
+++ controllers/player.js
@@ -14,6 +14,26 @@ module.exports.list = function (req, res, next) {
 	});
 };
 
+module.exports.favorites = function (req, res, next) {
+	'use strict';
+
+	var favoriteIds = [], i;
+
+	if (req.user.favorites && req.user.favorites.length > 0) {
+		for (i = 0; i < req.user.favorites.length; i += 1) {
+			favoriteIds.push(req.user.favorites[i].playerId);
+		}
+	}	
+
+	User.find({ '_id': { '$in': favoriteIds }}, function (err, user) {
+		if (err) {
+			return next(err);
+		}
+
+		return res.json(user);
+	});
+};
+
 module.exports.toggleFavorite = function (req, res, next) {
 	'use strict';
 
@@ -22,38 +42,30 @@ module.exports.toggleFavorite = function (req, res, next) {
 			return next(err);
 		}
 
-		if (user) {
-			User.findOne({ 'favorites.playerId': req.body.playerId }, function (err, favoriteUser) {
-				if (favoriteUser) {
-					user.favorites.pull({ 'favorites.playerId': req.body.playerId });
-				} else {
-					user.favorites.push({ playerId: mongoose.Types.ObjectId(req.body.playerId), associationDate: new Date() })
-				}
-				user.save(function (err, user) {
-					if (err) {
-						return next(err);
-					}
+		var i, favorite, found = false;
+		for (i = 0; i < user.favorites.length; i += 1) {
+			favorite = user.favorites[i];
 
-					if (user) {
-						return res.json(user);
-					}
-				});
-			});
+			if (String(favorite.playerId) === req.body.playerId) {
+			
+				user.favorites.splice(i, 1);
+				found = true;
+			}
 		}
-	});
 
-//	User.findByIdAndUpdate(
-//		req.user._id,
-//		{
-//			$push: {'favorites': { playerId: req.body.favoriteId, associationDate: new Date() }}
-//
-//		},
-//		function (err, user) {
-//			if (err) {
-//				return next(err);
-//			}
-//
-//			return res.json(user);
-//		}
-//	);
+		if (!found) {
+			user.favorites.push({ playerId: mongoose.Types.ObjectId(req.body.playerId), associationDate: new Date() });
+		}
+
+		user.save(function (err, user) {
+			if (err) {
+				return next(err);
+			}
+
+			if (user) {
+				return res.json(user);
+			}
+		});
+		
+	});
 };
--- routes/player.js
+++ routes/player.js
@@ -5,6 +5,7 @@ module.exports = function (app) {
     'use strict';
 
     app.get('/player/list', playerController.list);
+    app.get('/player/favorites', playerController.favorites);
     app.put('/player/favorite/toggle', playerController.toggleFavorite);
     
 };
